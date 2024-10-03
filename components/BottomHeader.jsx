da69a8f9e9cbb2ec34902e40bdf46833c0ffc86c
Felipe Cabrera <me@felieppe.com>
2024-10-03 18:16:18 -0300

    fix: replace importing of styles to fix components not using required CSS

--- components/BottomHeader.jsx
+++ components/BottomHeader.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse } from '@fortawesome/free-solid-svg-icons';
-import '../styles/BottomHeader.module.css';
+import styles from '../styles/BottomHeader.module.css';
 
 function BottomHeader() {
     return (
-        <div className="bottom-header">
-            <button className='homeButton'>
+        <div className={styles.bottom_header}>
+            <button className={styles.homeButton}>
                 <FontAwesomeIcon icon={faHouse} />
             </button>
-            <img id = "Foto" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNfOg5kIduOugqCXDAToZR-lViSJONXzAzoQ&s" alt="Foto de Perfil" />
+            <img id={styles.Foto} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNfOg5kIduOugqCXDAToZR-lViSJONXzAzoQ&s" alt="Foto de Perfil" />
         </div>
     )
 }
--- components/TopHeader.jsx
+++ components/TopHeader.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-regular-svg-icons';
 import { faSquarePlus } from '@fortawesome/free-regular-svg-icons';
-import '../styles/TopHeader.module.css';
+import styles from '../styles/TopHeader.module.css';
 
 function TopHeader() {
     return (
-        <div className="top-header">
+        <div className={styles.top_header}>
             <h1>Caffagram</h1>
-            <div className="button-group">
-                <button className='likeButton'>
+            <div className={styles.button_group}>
+                <button className={styles.likeButton}>
                     <FontAwesomeIcon icon={faHeart} />
                 </button>
-                <button className='createPost'>
+                <button className={styles.createPost}>
                     <FontAwesomeIcon icon={faSquarePlus} />
                 </button>
             </div>
