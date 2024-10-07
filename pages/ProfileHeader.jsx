bd33f0026a8571acf0bfc79faf03da7294e1b284
Iara <iara.vitabarhuber@gmail.com>
2024-10-07 10:30:27 -0300

    ahora si el css

--- pages/ProfileHeader.jsx
+++ pages/ProfileHeader.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import styles from  '../styles/ProfileHeader.module.css';
 
 function ProfileHeader({username, profilePicture, posts, friends, description}){
     return (
-        <div className="profile-header">
-            <img src={profilePicture} alt={`${username}'s profile`} className="profile-picture" />
-            <div className="profile-info">
+        <div className={styles["profile-header"]}>
+            <img src={profilePicture} alt={`${username}'s profile`} className={styles['profile-picture']} />
+            <div className={styles['profile-info']}>
                 <h1>{username}</h1>
                 <p>{description}</p>
-                <div className="stats">
+                <div className={styles['stats']}>
                     <span>{posts} Posts</span>
                     <span>{friends} Friends</span>
                 </div>
-                <button className="edit-profile-btn">Edit Profile</button>
+                <button className={styles['edit-profile-btn']}>Edit Profile</button>
             </div>
         </div>
     );
