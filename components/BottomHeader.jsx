91cdf62fc0b86d56f87265116702c7be65afcdd5
Stefano <stefano.francolino@correo.ucu.edu.uy>
2024-10-12 15:54:09 -0300

    MobileBottomButton Lo que hice fue que agregue un boton que te redirige a home, despues hay que modificar bien el tema de las rutas de ser necesario

--- components/BottomHeader.jsx
+++ components/BottomHeader.jsx
@@ -4,9 +4,13 @@ import { faHouse } from '@fortawesome/free-solid-svg-icons';
 import styles from '../styles/BottomHeader.module.css';
 
 function BottomHeader() {
+    const handleHomeClick = () => {
+        window.location.href = "http://localhost:3000/";
+    };
+    
     return (
         <div className={styles.bottom_header}>
-            <button className={styles.homeButton}>
+            <button onClick={handleHomeClick} className={styles.homeButton}>
                 <FontAwesomeIcon icon={faHouse} />
             </button>
             <img id={styles.Foto} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNfOg5kIduOugqCXDAToZR-lViSJONXzAzoQ&s" alt="Foto de Perfil" />
