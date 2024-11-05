155a9933abd44cdfea998631098f21f6be9cf89d
Felipe Cabrera <me@felieppe.com>
2024-11-05 19:19:11 -0300

    feat: implemented Upload Post Modal to Top Header component

--- components/TopHeader.jsx
+++ components/TopHeader.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import styles from '../styles/TopHeader.module.css';
+
+import CreatePostForm from './CreatePostForm';
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-regular-svg-icons';
 import { faSquarePlus } from '@fortawesome/free-regular-svg-icons';
-import styles from '../styles/TopHeader.module.css';
 
 function TopHeader() {
+    const [isCreatingPost, setIsCreatingPost] = useState(false);
+
+    const handleCreatePostClick = () => {
+        setIsCreatingPost(true);
+    };
+
+    const handleCloseCreatePost = () => {
+        setIsCreatingPost(false);
+    };
+
     return (
         <div className={styles.top_header}>
             <img src="/logo.svg" alt="Caffagram Logo" />
@@ -12,10 +24,11 @@ function TopHeader() {
                 <button className={styles.likeButton}>
                     <FontAwesomeIcon icon={faHeart} />
                 </button>
-                <button className={styles.createPost}>
+                <button className={styles.createPost} onClick={handleCreatePostClick}>
                     <FontAwesomeIcon icon={faSquarePlus} />
                 </button>
             </div>
+            {isCreatingPost && <CreatePostForm onClose={handleCloseCreatePost} />}
         </div>
     )
 }
