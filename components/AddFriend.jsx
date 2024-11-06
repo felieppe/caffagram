c197f53168480a45785484b99c898daaddb546f1
Stefano <stefano.francolino@correo.ucu.edu.uy>
2024-11-06 10:12:12 -0300

    ..

--- components/AddFriend.jsx
+++ components/AddFriend.jsx
@@ -1,29 +1,27 @@
 import React, { useState } from 'react';
-import { addFriendById} from '../utils/api';
+import { addFriendById } from '../utils/api';
 import styles from '../styles/AddFriend.module.css';
-
 
 const AddFriend = () => {
     const [isFriend, setIsFriend] = useState(false);
     const [message, setMessage] = useState("");
 
     const handleAddFriend = () => {
-        
         setIsFriend(true);
-        setMessage("¡Ya son amigos!"); 
+        setMessage("¡Ya son amigos!");
     };
 
     return (
         <div>
             {isFriend ? (
                 <div>
-                    <button disabled style={{ backgroundColor: 'lightgrey' }}>
+                    <button disabled className={styles['add-friend-btn']}>
                         Amigo Agregado
                     </button>
-                    <p>{message}</p> {/* Mostrar el mensaje de confirmación */}
+                    <p>{message}</p>
                 </div>
             ) : (
-                <button onClick={handleAddFriend}>
+                <button onClick={handleAddFriend} className={styles['add-friend-btn']}>
                     Agregar como Amigo
                 </button>
             )}
