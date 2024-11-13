faa838e3e8d299178e65795912a5a2652b2646d7
Felipe Cabrera <me@felieppe.com>
2024-11-13 12:31:24 -0300

    feat: add two side-menus in Feed View

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 import styles from '../styles/Feed.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEllipsis, faHeart as faFilledHeart } from '@fortawesome/free-solid-svg-icons';
+import { faBell, faEllipsis, faHeart as faFilledHeart, faHome, faInbox, faMagnifyingGlass, faUser } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as faEmptyHeart, faComment } from '@fortawesome/free-regular-svg-icons'
 import { useState, useContext } from 'react';
 import Image from 'next/image';
@@ -46,54 +46,84 @@ function Feed({ endpointPosts = [], jwt = '' }) {
         }
     }
 
+    const handleLogout = () => {
+        document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+        localStorage.removeItem('user');
+        window.location.href = '/Login';
+    }
+
     if (!user) { return <div>Loading...</div>; }
 
     return (
         <>
             <TopHeader />
 
-            <div className={styles.feed}>
-                <div className={styles.posts}>
-                    {posts.map(post => (
-                        <div key={post._id} data-id={post._id} className={styles.post}>
-                            <div className={styles.post__top}>
-                                <div className={styles.post__top__user}>
-                                    <Link href={`/${post.user.username}`}>
-                                        {/* I suppose this wont work. Backend does not server images URL well, just gives source path. */}
-                                        {/* It is not performance-friendly to fetch twice the profile just for getting the profile picture URL. */}
-                                        <Image className={styles.post__top__user__img} src={fetchProfileById(post.user._id, jwt).profilePicture ? fetchProfileById(post.user._id, jwt).profilePicture : "/default-profile.webp"} alt="User" width={30} height={30} />
-                                    </Link>
-                                    <p>@{post.user.username}</p>
+            <div className={styles.container}>
+                <div className={styles.left__menu}>
+                    <ul>
+                        {/* Near every Link redirects to / because not handler of that pages are created. */}
+                        <li className={styles.left__menu__active}><Link href="/"><FontAwesomeIcon icon={faHome}/> Home</Link></li>
+                        <li><Link href="/"><FontAwesomeIcon icon={faMagnifyingGlass} /> Explore</Link></li>
+                        <li><Link href="/"><FontAwesomeIcon icon={faInbox} /> Messages</Link></li>
+                        <li><Link href="/"><FontAwesomeIcon icon={faBell} /> Notifications</Link></li>
+                        <li><Link href={`/` + user.username}><FontAwesomeIcon icon={faUser} /> Profile</Link></li>
+                    </ul>
+                </div>
+
+                <div className={styles.feed}>
+                    <div className={styles.posts}>
+                        {posts.map(post => (
+                            <div key={post._id} data-id={post._id} className={styles.post}>
+                                <div className={styles.post__top}>
+                                    <div className={styles.post__top__user}>
+                                        <Link href={`/${post.user.username}`}>
+                                            {/* I suppose this wont work. Backend does not server images URL well, just gives source path. */}
+                                            {/* It is not performance-friendly to fetch twice the profile just for getting the profile picture URL. */}
+                                            <Image className={styles.post__top__user__img} src={fetchProfileById(post.user._id, jwt).profilePicture ? fetchProfileById(post.user._id, jwt).profilePicture : "/default-profile.webp"} alt="User" width={30} height={30} />
+                                        </Link>
+                                        <p>@{post.user.username}</p>
+                                    </div>
+
+                                    <div className={styles.post__top__options}>
+                                        <FontAwesomeIcon icon={faEllipsis} />
+                                    </div>
                                 </div>
 
-                                <div className={styles.post__top__options}>
-                                    <FontAwesomeIcon icon={faEllipsis} />
+                                <div className={styles.post__image}>
+                                    <Image src={"http://localhost:3001/" + post.imageUrl} alt="Post" width={350} height={300} />
+                                </div>
+
+                                <div className={styles.post__actions}>
+                                    <FontAwesomeIcon icon={post.likes.includes(post.user._id) ? faFilledHeart : faEmptyHeart} className={post.likes.includes(user.id) ? styles.post__liked : null} onClick={ () => { handleLike(post._id) } }/>
+                                    <FontAwesomeIcon icon={faComment} />
+                                </div>
+
+                                <div className={styles.post__likes}>
+                                    {post.likes.length} Likes
+                                </div>
+
+                                <div className={styles.post__description}>
+                                    <b>{post.user.username}</b>
+                                    <p>{post.caption}</p>
+                                </div>
+
+                                <div className={styles.post__comments}>
+                                    { post.comments.length > 0 ? `View all ${post.comments.length} comments` : null}
                                 </div>
                             </div>
+                        ))}
+                    </div>
+                </div>
 
-                            <div className={styles.post__image}>
-                                <Image src={"http://localhost:3001/" + post.imageUrl} alt="Post" width={350} height={300} />
-                            </div>
+                <div className={styles.right__menu}>
+                    <div className={styles.right__menu__user}>
+                        <Image src={fetchProfileById(user.id, jwt).profilePicture ? fetchProfileById(user.id, jwt).profilePicture : "/default-profile.webp"} alt="User" width={35} height={35} />
+                        @{user.username}
 
-                            <div className={styles.post__actions}>
-                                <FontAwesomeIcon icon={post.likes.includes(post.user._id) ? faFilledHeart : faEmptyHeart} className={post.likes.includes(user.id) ? styles.post__liked : null} onClick={ () => { handleLike(post._id) } }/>
-                                <FontAwesomeIcon icon={faComment} />
-                            </div>
-
-                            <div className={styles.post__likes}>
-                                {post.likes.length} Likes
-                            </div>
-
-                            <div className={styles.post__description}>
-                                <b>{post.user.username}</b>
-                                <p>{post.caption}</p>
-                            </div>
-
-                            <div className={styles.post__comments}>
-                                { post.comments.length > 0 ? `View all ${post.comments.length} comments` : null}
-                            </div>
+                        <div className={styles.right__menu__user__logout}>
+                            <Link href="/Login" onClick={handleLogout}>Logout</Link>
                         </div>
-                    ))}
+                    </div>
                 </div>
             </div>
 
