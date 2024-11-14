37a34b700946b9085fb06122de219e35d45ddfcf
Felipe Cabrera <me@felieppe.com>
2024-11-14 12:51:13 -0300

    refactor: remove faHeart button from Top Header

--- components/TopHeader.jsx
+++ components/TopHeader.jsx
@@ -3,7 +3,6 @@ import styles from '../styles/TopHeader.module.css';
 import CreatePostForm from './modals/CreatePostForm';
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart } from '@fortawesome/free-regular-svg-icons';
 import { faSquarePlus } from '@fortawesome/free-regular-svg-icons';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -30,9 +29,6 @@ function TopHeader() {
         <div className={styles.top_header}>
             <Link href={"/"}><Image src="/logo.svg" alt="Caffagram Logo" width={120} height={120} /></Link>
             <div className={styles.button_group}>
-                <button className={styles.likeButton}>
-                    <FontAwesomeIcon icon={faHeart} />
-                </button>
                 <button className={styles.createPost} onClick={handleCreatePostClick}>
                     <FontAwesomeIcon icon={faSquarePlus} />
                 </button>
